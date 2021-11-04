52a2edfa995a1086f7ba87347cd2502003fb25ba
abbasnaumani <abbasnaumani@gmail.com>
2021-11-04 19:07:01 +0500

    auth lgoic for sidebar updated

--- resources/js/layouts/Default.vue
+++ resources/js/layouts/Default.vue
@@ -1,16 +1,17 @@
 <template>
-    <div id="page-container" class="sidebar-o enable-page-overlay sidebar-dark side-scroll page-header-fixed main-content-narrow side-trans-enabled">
-        <div v-if="isAuthenticated()">
+    <div id="page-container"
+         class="sidebar-o enable-page-overlay sidebar-dark side-scroll page-header-fixed main-content-narrow side-trans-enabled">
+        <div v-if="isAuthenticated">
             <AuthHeader></AuthHeader>
-            <sidebar />
+            <sidebar/>
         </div>
-<!--                <slot name="top-bar" />-->
-<!--        <user-dashboard></user-dashboard>-->
+        <!--                <slot name="top-bar" />-->
+        <!--        <user-dashboard></user-dashboard>-->
         <child/>
-<!--        <component v-bind:is="'userDashboard'"></component>-->
-<!--                <slot name="main-content" />-->
-<!--        <UserList></UserList>-->
-<!--        <Login></Login>-->
+        <!--        <component v-bind:is="'userDashboard'"></component>-->
+        <!--                <slot name="main-content" />-->
+        <!--        <UserList></UserList>-->
+        <!--        <Login></Login>-->
     </div>
 </template>
 
@@ -22,14 +23,15 @@ import Sidebar from "../components/ui/base/Sidebar";
 import Child from "../components/utilities/Child";
 import auth from "../services/authService";
 import store from "../store";
+import {computed} from "vue";
 
 export default {
     name: "Default",
-    components: { Child, Sidebar, AuthHeader, Login},
+    components: {Child, Sidebar, AuthHeader, Login},
     setup(props) {
-        function  isAuthenticated(){
-            return auth.isAuthenticated()
-        }
+        const isAuthenticated = computed(() => {
+            return store.getters.isAuthenticated ? store.getters.isAuthenticated : null;
+        });
         return {
             isAuthenticated
         }
