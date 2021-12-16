b0072533c51097b9cb44b48498fffda06ea3d4fb
Abbas Naumani <abbasnaumani@gmail.com>
2021-12-16 18:38:55 +0500

    Frontsite default component added

--- resources/js/pages/frontsite/property/Properties.vue
+++ resources/js/pages/frontsite/property/Properties.vue
@@ -420,17 +420,6 @@
                             </div>
                         </div>
                     </div>
-<!--                    <div class="row">-->
-<!--                        <div class="d-flex mx-auto pt-3">-->
-<!--                            <ul class="pagination">-->
-<!--                                <li class="page-item"><a class="page-link" href="#"><i class="fas fa-angle-left"></i></a></li>-->
-<!--                                <li class="page-item bg-primary active"><a class="page-link" href="#">1</a></li>-->
-<!--                                <li class="page-item "><a class="page-link" href="#">2</a></li>-->
-<!--                                <li class="page-item"><a class="page-link" href="#">3</a></li>-->
-<!--                                <li class="page-item"><a class="page-link" href="#"><i class="fas fa-angle-right"></i></a></li>-->
-<!--                            </ul>-->
-<!--                        </div>-->
-<!--                    </div>-->
                 </div>
             </div>
         </div>
