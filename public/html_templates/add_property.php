d13964ff11a624c2c2d016e27b4b655d6fc9ee80
bilalhaider1122 <bilal.kodestudio@gmail.com>
2021-12-20 21:06:06 +0500

    font weight testing

---
+++ public/html_templates/add_property.php
@@ -0,0 +1,11 @@
+<?php include "header.php" ?>
+<section id="add_property">
+    <div class="container py-5">
+        <div class="add_property_page mt-5">
+            <form action="">
+                <h1 class="font-weight-bold add_property">Add a Property</h1>
+            </form>
+        </div>
+    </div>
+</section>
+<?php include "footer.php" ?>
