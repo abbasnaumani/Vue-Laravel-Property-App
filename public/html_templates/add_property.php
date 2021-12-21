d076b683889a2484e0a1b0acb9ed0c483d579ed8
Mudassar Hashmi <mudassar.kodestudio@gmail.com>
2021-12-21 21:16:15 +0500

    Add property page modify

--- public/html_templates/add_property.php
+++ public/html_templates/add_property.php
@@ -1,10 +1,144 @@
 <?php include "header.php" ?>
 <section id="add_property">
     <div class="container py-5">
+        <div class="card shadow-sm login-card w-100 text-left mt-5">
+            <div class="card-header bg-white border-bottom-0 mt-4">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h2 class="font-weight-bold add_property text-3 px-3">Add a Property</h2>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="row px-3">
+                    <div class="col-lg-12">
+                        <form id="user-profile" action="" method="">
+                            <div class="main_subheader" style="margin-top: -30px;">Property Type and Location</div>
+
+                            <div class="form-group row pt-2">
+                                <label class="col-lg-3 text-right line-height-9 text-2">Purpose:</label>
+                                <div class="col-lg-9">
+                                    <div class="d-flex align-items-center">
+                                        <div class="btn btn-primary border-right">
+                                            <div class="form-check">
+                                                <input type="radio" class="form-check-input" id="" name="materialExampleRadios">
+                                                <label class="form-check-label" for="materialUnchecked">For Sale</label>
+                                            </div>
+                                        </div>
+                                        <div class="btn btn-primary">
+                                            <div class="form-check">
+                                                <input type="radio" class="form-check-input" id="" name="materialExampleRadios">
+                                                <label class="form-check-label" for="materialUnchecked">Rent</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group row pt-2">
+                                <label class="col-lg-3 text-right line-height-9 text-2">Property Type:</label>
+                                <div class="col-lg-9">
+                                    <div class="d-flex align-items-center">
+                                        <div class="btn btn-primary border-right">
+                                            <div class="form-check ">
+                                                <input type="radio" class="form-check-input" id="" name="propertytype" type="button" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
+                                                <label class="form-check-label " for="materialUnchecked">Homes</label>
+                                            </div>
+                                        </div>
+                                        <div class="btn btn-primary border-right">
+                                            <div class="form-check ">
+                                                <input type="radio" class="form-check-input" id="" name="propertytype" data-toggle="collapse" data-target=".multi-collapse01" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
+                                                <label class="form-check-label" for="materialUnchecked">Plotes</label>
+                                            </div>
+                                        </div>
+                                        <div class="btn btn-primary border-right">
+                                            <div class="form-check">
+                                                <input type="radio" class="form-check-input" id="" name="propertytype" data-toggle="collapse" data-target=".multi-collapse02" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">
+                                                <label class="form-check-label" for="materialUnchecked">Commerical</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- drop data -->
+                                    <div class="row w-50 ">
+                                        <div class="col ">
+                                            <div class="collapse multi-collapse mt-3" id="">
+                                                <div class="btn btn-primary">
+                                                    <div class="form-check">
+                                                        <input type="radio" class="form-check-input" id="" name="materialExampleRadios01">
+                                                        <label class="form-check-label">For Sale</label>
+                                                    </div>
+                                                </div>
+                                                <div class="btn btn-primary">
+                                                    <div class="form-check">
+                                                        <input type="radio" class="form-check-input" id="" name="materialExampleRadios01">
+                                                        <label class="form-check-label">Rent</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex">
+                                        <div class="collapse multi-collapse01 mt-3 " id="">
+                                            <div class="btn btn-primary">
+                                                <div class="form-check">
+                                                    <input type="radio" class="form-check-input" id="" name="materialExampleRadios02">
+                                                    <label class="form-check-label">House</label>
+                                                </div>
+                                            </div>
+                                            <div class="btn btn-primary">
+                                                <div class="form-check">
+                                                    <input type="radio" class="form-check-input" id="" name="materialExampleRadios02">
+                                                    <label class="form-check-label">Flate</label>
+                                                </div>
+                                            </div>
+                                            <div class="btn btn-primary">
+                                                <div class="form-check">
+                                                    <input type="radio" class="form-check-input" id="" name="materialExampleRadios02">
+                                                    <label class="form-check-label">Room</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="d-flex align-item-center">
+                                        <div class="collapse multi-collapse02 mt-3 " id="">
+                                            <div class="btn btn-primary">
+                                                <div class="form-check">
+                                                    <input type="radio" class="form-check-input" id="" name="materialExampleRadios02">
+                                                    <label class="form-check-label">Office</label>
+                                                </div>
+                                            </div>
+                                            <div class="btn btn-primary">
+                                                <div class="form-check">
+                                                    <input type="radio" class="form-check-input" id="" name="materialExampleRadios02">
+                                                    <label class="form-check-label">Shop</label>
+                                                </div>
+                                            </div>
+                                            <div class="btn btn-primary">
+                                                <div class="form-check">
+                                                    <input type="radio" class="form-check-input" id="" name="materialExampleRadios02">
+                                                    <label class="form-check-label">Building</label>
+                                                </div>
+                                            </div>
+                                            <div class="btn btn-primary">
+                                                <div class="form-check">
+                                                    <input type="radio" class="form-check-input" id="" name="materialExampleRadios02">
+                                                    <label class="form-check-label">Other</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container py-5">
         <div class="row">
             <div class="col-lg-9">
                 <h2 class="font-weight-bold add_property">Add a Property</h2>
-                <div class="req_label"><span class="required"></span>=Required Fields.</div>
                 <div class="main_subheader">Property Type and Location</div>
                 <div class="add_property_page mt-3 d-flex justify-content-center">
                     <form action="" class="">
